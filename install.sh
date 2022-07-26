3d7cc6d88534df88816d26773c46c6802c880b20
OnHai <55747660+Adrianahwjsgsj@users.noreply.github.com>
2022-07-26 13:47:17 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo apt install python3-pip ; sudo apt install python3 ; git clone https://github.com/ultralytics/yolov5 ; pip install numpy ; pip install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt ; pip install opencv-python ; pip install picamera
