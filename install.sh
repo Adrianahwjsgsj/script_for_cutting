a4306ebca2e42326384f5c01a79386aefe6b771c
OnHai <55747660+Adrianahwjsgsj@users.noreply.github.com>
2022-08-02 11:49:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-sudo apt install python3-pip | sudo apt install python3 | git clone https://github.com/ultralytics/yolov5 | pip3 install numpy | pip3 install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt | pip3 install opencv-python | pip3 install picamera
+sudo apt install python3-pip | sudo apt install python3 | git clone https://github.com/ultralytics/yolov5 | pip3 install numpy | pip3 install -qr https://raw.githubusercontent.com/ultralytics/yolov5/master/requirements.txt | pip3 install opencv-python | pip3 install picamera | echo "neco se nainstalovalo" 
